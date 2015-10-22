b356ce6600db59aef192fd10c8d06910e2dd32b5
Vinicius <vinicius.mascarenhas@icloud.com>
2015-10-22 03:59:00 -0200

    Corrigindo o loop.

--- Calculo de Retangulo.cpp
+++ Calculo de Retangulo.cpp
@@ -2,28 +2,27 @@
 
 using namespace std;
 
-    int main ()
-	{	
-	    float a,b;
-	    
-	    cout<< "Informe a medida da base do retangulo(cm): ";
-	    cin>> a;
-	    cout<<"\nInforme a medida da altura do retangulo(cm): ";
-	    cin>> b;
-	    
-	    cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm2\n";	    
-	    
-		for(;;a!=b)	    
-		{
-			cout<<"\nEsse quadrilatero e um quadrado!\nInforme a medida da base do retangulo(cm): ";
-			cin>> a;
-	    	cout<<"\nInforme a medida da altura do retangulo(cm): ";
-	    	cin>> b;
-	    
-	    	cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm²";
-			
-	   	}
-	   	
-		return 0;	    
-	    
+int main ()
+{	
+	float a,b;
+	
+	cout<< "Informe a medida da base do retangulo(cm): ";
+	cin>> a;
+	cout<<"\nInforme a medida da altura do retangulo(cm): ";
+	cin>> b;
+	
+	cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm2\n";	    
+	
+	while(a==b)    
+	{
+		cout<<"\nEsse quadrilatero e um quadrado!\nInforme a medida da base do retangulo(cm): ";
+		cin>> a;
+		cout<<"\nInforme a medida da altura do retangulo(cm): ";
+		cin>> b;
+	
+		cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm2";
 	}
+	
+	return 0;	    
+	
+}
