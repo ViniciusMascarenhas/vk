c358dc405c359817e9a9087d92c92e135e65f889
nuneskayan <nunes.kayan@gmail.com>
2015-10-22 04:14:54 -0200

    Corrigindo o loop de novo. do { } while ( )

--- Calculo de Retangulo.cpp
+++ Calculo de Retangulo.cpp
@@ -6,22 +6,19 @@ int main ()
 {	
 	float a,b;
 	
-	cout<< "Informe a medida da base do retangulo(cm): ";
-	cin>> a;
-	cout<<"\nInforme a medida da altura do retangulo(cm): ";
-	cin>> b;
-	
-	cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm2\n";	    
-	
-	while(a==b)    
+	do
 	{
-		cout<<"\nEsse quadrilatero e um quadrado!\nInforme a medida da base do retangulo(cm): ";
+		cout<<"\nInforme a medida da base do retangulo(cm): ";
 		cin>> a;
 		cout<<"\nInforme a medida da altura do retangulo(cm): ";
 		cin>> b;
 	
 		cout<<"\nA area do seu retangulo e de: "<<a*b<<"cm2";
+		
+		if (a==b) cout << "Esse quadrilatero e um quadrado!\n";
 	}
+	while(a==b);    
+	
 	
 	return 0;	    
 	
