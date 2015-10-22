186bd0ff05cbd7e2bb9533facc930d90db1e7c34
Vinicius <vinicius.mascarenhas@icloud.com>
2015-10-22 05:20:03 -0200

    Experiência e performance em um jogo

---
+++ Exp.cpp
@@ -0,0 +1,19 @@
+/*
+um laboratório de desenvolvimento de jogos solicitou um algoritimo de classificação de permonca para um determinado jogo. o algoritimo devera classificar os jogadores de acordo com a tabela abaixo, sendo que performance e calculada pela experiência acumulada pelo jogador dividida pelo nuero de vezes q jogou. O algoritimo deve solicitar o nível de exp e a quantidade de vezes que o jogador jogou. De acordo com a tabela abaixo, o algoritimo mostra a classificação e pergunta s eexiste mais jogadores para classificar. O programa ira ficar no "laço" ate que a resposta seja negativa
+0-500 ( iniciante)
+501-2500(normal)
+>2500(experiente)
+ */
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	
+	
+	
+	
+	return 0;
+}
