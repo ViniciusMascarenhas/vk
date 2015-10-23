770b381fd852898a731be22a4e7da17d209fd462
nuneskayan <nunes.kayan@gmail.com>
2015-10-23 06:24:25 -0200

    calculo fibonacci

--- Calculo de Fibonacci.cpp
+++ Calculo de Fibonacci.cpp
@@ -1,36 +1,54 @@
-//Calcule a série de Fibonacci para um número inteiro não negativo informado pelo
-//usuário. A série de Fibonacci inicia com os números F0 = 0 e F1 = 1, e cada número
-//posterior equivale à soma dos dois números anteriores (Fn = Fn-1 + Fn-2). Por exemplo,
-//caso o usuário informe o número 9, o resultado seria: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
-
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include <ctime>
-#include <cstdlib>
 
 
 using namespace std;
 
-            int main () {
-            	
-            	int x,f0,f1;
-            	 
-            	
-            	cout<< "Informe um numero inteiro nao negativo para começar a brincadeira putinha:  "<< "\n";
-            	cin>> x;
-            	f0=0;
-            	f1-1;
-            	x=f1+f0;
-            	
-            	
-            	
-            	
-            	
-        
+int fib (int n)
+{
+	if(n==0)
+	return 0;
+	
+	if(n==1)
+	return 1;
+	
+	return fib(n-1)+fib(n-2);
+			
+	
+};
+
+int main (){
+	
+	int n;
+	
+	while (n>=0)
+	
+	{
+	
+		cout<<"Informe um numero para calcular o seu valor pelo calculo de fibonacci: ";
+		cin>>n;
+	
+		if (n<0)
+		{	
+			
+			cout<<"\nEsse numero e negativo, entre com um numero positivo para continuar: ";
+			cin>>n;
+					
+			if (n<0)
+			{
 		
-		system ("pause");
-        
+				cout<<"O numero continua invalido, o programa ira ser fechado.";
+				return 1;
+										
 			}
-
-
+		}
+	
+	cout<<"\nO seu numero calculado atraves de Fibonacci e igual a: "<<fib(n);
+	cout<<"\n\n";
+	
+	
+	}
+	
+	return 0;
+	
+	
+}
