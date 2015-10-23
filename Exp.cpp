10df93c6405f068daf5444aaa288b936a4b69084
Vinicius <vinicius.mascarenhas@icloud.com>
2015-10-23 02:25:51 -0200

    Organizando um pouco.

--- Exp.cpp
+++ Exp.cpp
@@ -1,19 +1,18 @@
+#include <stdio.h>		// Para printf e scanf.
+#include <iostream>		// Para std::cout e std::cin.
+using namespace std;	// Para não precisar digitar std:: toda vez que for usar cout e cin.
+
 /*
-um laboratório de desenvolvimento de jogos solicitou um algoritimo de classificação de permonca para um determinado jogo. o algoritimo devera classificar os jogadores de acordo com a tabela abaixo, sendo que performance e calculada pela experiência acumulada pelo jogador dividida pelo nuero de vezes q jogou. O algoritimo deve solicitar o nível de exp e a quantidade de vezes que o jogador jogou. De acordo com a tabela abaixo, o algoritimo mostra a classificação e pergunta s eexiste mais jogadores para classificar. O programa ira ficar no "laço" ate que a resposta seja negativa
-0-500 ( iniciante)
-501-2500(normal)
->2500(experiente)
+ Um laboratório de desenvolvimento de jogos solicitou um algoritimo de classificação de performance para um determinado jogo. O algoritimo deverá classificar os jogadores de acordo com a tabela abaixo, sendo que performance é calculada pela experiência acumulada pelo jogador dividida pelo número de vezes que jogou. O algoritimo deve solicitar o nível de experiência e a quantidade de vezes que o jogador jogou. De acordo com a tabela abaixo, o algoritimo mostra a classificação e pergunta se existem mais jogadores para classificar. O programa irá ficar no "laço" até que a resposta seja negativa.
+ 
+ 0 a 500	(iniciante)
+ 501 a 2500	(normal)
+ > 2500		(experiente)
  */
 
-#include <stdio.h>
-#include <iostream>
-using namespace std;
 
 int main ()
 {
 	
-	
-	
-	
-	return 0;
 }
+
