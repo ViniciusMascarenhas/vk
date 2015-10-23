7bcaf4a1dc6d93563331d9734648488ec333decc
Vinicius <vinicius.mascarenhas@icloud.com>
2015-10-23 06:31:09 -0200

    Corrigido.

--- TextoCriptografado+3.cpp
+++ TextoCriptografado+3.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <stdio.h>
-
+#include <string.h>
 using namespace std;
 
-int main (){
+int main ()
+{
 	
 	int x;
-	char texto[999];
+	//char texto[999];
+	string texto;
 	
 	cout<<"Escreve o texto a ser criptografado: ";
 	cin>>texto;
 	
-	for(x=0;texto[x]!='\0';x++)
 	
-		texto[x]=x+3;
+	for(x=0; texto[x]!='\0'; x++)
+	{
+		texto[x]=texto[x]+3;
+	};
 		
-	cout<<"\nSeu texto criptografado e esse-> "<<texto;
+	cout<<"\nSeu texto criptografado e esse:\n"<<texto<<"\n\n";
 	
 								
 				
