978f2192a9ad1588575697ad260c2ee8aa57c2bd
nuneskayan <nunes.kayan@gmail.com>
2015-10-25 04:48:43 -0200

    7Luck 1.01

---
+++ 7Luck.cpp
@@ -0,0 +1,27 @@
+/* escolha um numero e jogue um dado
+o programa deve informa se o numero q voce escolheu saiu ou nao
+caso nao tenha saido continuar jogando os dois dados,
+ate sair o numero desejado.
+Informar quantas vezes o dado foi jogado*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h
+
+using namespace std;
+
+int main(){
+	
+	srand(time(NULL));
+	
+	int n;
+	
+	cout<<"vamos jogar 7Luck escoha um numero de 2 a 12:  ";
+	cin>>n;
+	
+	
+	
+	
+	
+	
+}
